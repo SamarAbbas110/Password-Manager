feb486112ceb87ebc5f526d29cc7d51b06a09750
Samar Abbas <71007148+SamarAbbas110@users.noreply.github.com>
2025-02-21 11:20:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import React from "react";
 import Navbar from './Components/Navbar/Navbar.'
 import Main from './Components/Main/Main'
 import Footer from './Components/Footer/Footer'
+
 
 function App() {
 
